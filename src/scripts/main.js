2ddf22492a8301bfd256743a9991b186284b014f
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-04-23 09:04:57 -0500

    updated fetch request method

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,4 @@
-import "./modules/API-meetups.js";
+import "./modules/API-concerts.js";
+// import "./modules/API-restaurants.js";
+// import "./modules/API-meetups.js";
+// import "./modules/API-parks.js";
--- src/scripts/modules/API-concerts.js
+++ src/scripts/modules/API-concerts.js
@@ -1,10 +1,35 @@
 console.log("Hello, World!");
+import APIkeys from "../../../env/apiKeys.js";
 
-function searchConcerts() {
-    fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=VljGRC0DhTR15ecGRXfkPFXQkBSiqh0x")
-      .then(events => events.json())
-      .then(event => {
-          console.table(event);
-        });
-  };
-  searchConcerts();
+// concertCard = (concert) => {
+//   return `<div class="card-full">
+//             <div class="card-image">
+//                 <img src="#" alt="venueImage">
+//             </div>
+//             <div class="card-content">
+//                 <h1>EventTitle</h1>
+//                 <h3>Date/Time</h3>
+//                 <h3>Location</h3>
+//             </div>
+//             <div class="card-contentExtended">
+//                 <p>Some Content</p>
+//             </div>
+//           </div>`
+// };
+
+// Set up to only search in the nashville area / Can easily change to make radius set to different city by making dma ID change functionality
+function searchConcerts(input) {
+  fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${input}&classificationName=music&dmaId=343&apikey=${APIkeys.concerts}`)
+    .then(events => events.json())
+    .then(event => {
+      event._embedded.events.forEach(item => {
+        console.log(`${item.name}`, item._embedded.venues[0].name);
+      });
+    });
+};
+searchConcerts("hip hop");
+
+// const inputValue = document.querySelector("#concertsBtn").addEventListener("click", function(event){
+//  return document.querySelector("inputField").value;
+// });
+// searchConcerts(inputValue);
