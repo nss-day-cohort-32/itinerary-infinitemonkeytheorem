072c5ac115e2b622c55badcf74e6a37ef9ebab09
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-04-24 08:59:26 -0500

    finished itinerary add button

--- src/scripts/modules/itineraryButton.js
+++ src/scripts/modules/itineraryButton.js
@@ -2,29 +2,31 @@
 document.querySelector("#root").addEventListener("click", addToItinerary);
 
 function addToItinerary(event) {
-  // select necessary card values
-  let itineraryItem = event.target.parentElement.closest(".mdc-card").querySelector("#cardTitle").innerHTML;
-  let itineraryItemLocale = event.target.parentElement.closest(".mdc-card").querySelector("#eventLocation").innerHTML;
-  let itineraryItemType = event.target.parentElement.closest(".mdc-card").id.split("--")[1].split("-")[0];
+  if (event.srcElement.nodeName === "BUTTON") {
+    // select necessary card values
+    let itineraryItem = event.target.parentElement.closest(".mdc-card").querySelector("#cardTitle").innerHTML;
+    let itineraryItemLocale = event.target.parentElement.closest(".mdc-card").querySelector("#eventLocation").innerHTML;
+    let itineraryItemType = event.target.parentElement.closest(".mdc-card").id.split("--")[1].split("-")[0];
 
-  // select necessary dom elements
-  let myItinerary = document.querySelector("#itinerary");
-  let searchItems = document.querySelector("#root");
+    // select necessary dom elements
+    let myItinerary = document.querySelector("#itinerary");
+    let searchItems = document.querySelector("#root");
 
-  // create div element to contain selected events
-  let myItineraryContent = document.createElement("div");
+    // create div element to contain selected events
+    let myItineraryContent = document.createElement("div");
 
-  // clear all search results on selecting card
-  searchItems.innerHTML = "";
+    // clear all search results on selecting card
+    searchItems.innerHTML = "";
 
-  // appends new div into itinerary container
-  myItinerary.appendChild(myItineraryContent);
+    // appends new div into itinerary container
+    myItinerary.appendChild(myItineraryContent);
 
-  // sets event item html format
-  let listing = document.createTextNode(`${itineraryItemType}: ${itineraryItem} at ${itineraryItemLocale}`);
+    // sets event item html format
+    let listing = document.createTextNode(`${itineraryItemType}: ${itineraryItem} at ${itineraryItemLocale}`);
 
-  // append new element to appended div element
-  myItineraryContent.appendChild(listing);
+    // append new element to appended div element
+    myItineraryContent.appendChild(listing);
+  };
 };
 
 // This function will need to be alter to add items to json database as well as use a PUT method to replace old cards with new selects
